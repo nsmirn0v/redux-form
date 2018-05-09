5ac832735a8e5b7a9d585aaa146bd387df3b5dcd
Nikolay <nsmirnov.y@gmail.com>
2018-05-09 11:39:30 -0700

    Update prop-types

--- src/createAll.js
+++ src/createAll.js
@@ -1,48 +1,57 @@
-import reducer from './reducer';
-import createReduxForm from './createReduxForm';
-import mapValues from './mapValues';
-import bindActionData from './bindActionData';
-import * as actions from './actions';
-import * as actionTypes from './actionTypes';
-import createPropTypes from './createPropTypes';
-import getValues from './getValuesFromState';
+import PropTypes from 'prop-types'
+import reducer from './reducer'
+import createReduxForm from './createReduxForm'
+import mapValues from './mapValues'
+import bindActionData from './bindActionData'
+import * as actions from './actions'
+import * as actionTypes from './actionTypes'
+import createPropTypes from './createPropTypes'
+import getValues from './getValuesFromState'
 
 // bind form as first parameter of action creators
 const boundActions = {
-  ...mapValues({
-    ...actions,
-    autofillWithKey: (key, ...args) => bindActionData(actions.autofill, {key})(...args),
-    changeWithKey: (key, ...args) => bindActionData(actions.change, {key})(...args),
-    initializeWithKey: (key, ...args) => bindActionData(actions.initialize, {key})(...args),
-    reset: (key) => bindActionData(actions.reset, {key})(),
-    touchWithKey: (key, ...args) => bindActionData(actions.touch, {key})(...args),
-    untouchWithKey: (key, ...args) => bindActionData(actions.untouch, {key})(...args),
-    destroy: (key) => bindActionData(actions.destroy, {key})()
-  }, action => (form, ...args) => bindActionData(action, {form})(...args))
-};
+  ...mapValues(
+    {
+      ...actions,
+      autofillWithKey: (key, ...args) =>
+        bindActionData(actions.autofill, { key })(...args),
+      changeWithKey: (key, ...args) =>
+        bindActionData(actions.change, { key })(...args),
+      initializeWithKey: (key, ...args) =>
+        bindActionData(actions.initialize, { key })(...args),
+      reset: key => bindActionData(actions.reset, { key })(),
+      touchWithKey: (key, ...args) =>
+        bindActionData(actions.touch, { key })(...args),
+      untouchWithKey: (key, ...args) =>
+        bindActionData(actions.untouch, { key })(...args),
+      destroy: key => bindActionData(actions.destroy, { key })(),
+    },
+    action => (form, ...args) => bindActionData(action, { form })(...args)
+  ),
+}
 
-const addArrayValue = boundActions.addArrayValue;
-const autofill = boundActions.autofill;
-const autofillWithKey = boundActions.autofillWithKey;
-const blur = boundActions.blur;
-const change = boundActions.change;
-const changeWithKey = boundActions.changeWithKey;
-const destroy = boundActions.destroy;
-const focus = boundActions.focus;
-const initialize = boundActions.initialize;
-const initializeWithKey = boundActions.initializeWithKey;
-const removeArrayValue = boundActions.removeArrayValue;
-const reset = boundActions.reset;
-const startAsyncValidation = boundActions.startAsyncValidation;
-const startSubmit = boundActions.startSubmit;
-const stopAsyncValidation = boundActions.stopAsyncValidation;
-const stopSubmit = boundActions.stopSubmit;
-const submitFailed = boundActions.submitFailed;
-const swapArrayValues = boundActions.swapArrayValues;
-const touch = boundActions.touch;
-const touchWithKey = boundActions.touchWithKey;
-const untouch = boundActions.untouch;
-const untouchWithKey = boundActions.untouchWithKey;
+const addArrayValue = boundActions.addArrayValue
+const autofill = boundActions.autofill
+const autofillWithKey = boundActions.autofillWithKey
+const blur = boundActions.blur
+const change = boundActions.change
+const changeWithKey = boundActions.changeWithKey
+const destroy = boundActions.destroy
+const focus = boundActions.focus
+const initialize = boundActions.initialize
+const initializeWithKey = boundActions.initializeWithKey
+const removeArrayValue = boundActions.removeArrayValue
+const reset = boundActions.reset
+const startAsyncValidation = boundActions.startAsyncValidation
+const startSubmit = boundActions.startSubmit
+const stopAsyncValidation = boundActions.stopAsyncValidation
+const stopSubmit = boundActions.stopSubmit
+const submitFailed = boundActions.submitFailed
+const swapArrayValues = boundActions.swapArrayValues
+const touch = boundActions.touch
+const touchWithKey = boundActions.touchWithKey
+const untouch = boundActions.untouch
+const untouchWithKey = boundActions.untouchWithKey
 
 export default function createAll(isReactNative, React, connect) {
   return {
@@ -58,7 +67,7 @@ export default function createAll(isReactNative, React, connect) {
     getValues,
     initialize,
     initializeWithKey,
-    propTypes: createPropTypes(React),
+    propTypes: createPropTypes(PropTypes),
     reduxForm: createReduxForm(isReactNative, React, connect),
     reducer,
     removeArrayValue,
@@ -72,6 +81,6 @@ export default function createAll(isReactNative, React, connect) {
     touch,
     touchWithKey,
     untouch,
-    untouchWithKey
-  };
+    untouchWithKey,
+  }
 }
